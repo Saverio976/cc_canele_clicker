317eddc43c2800926d4ec0ab015cc138c0215674
Saverio <personnex976@gamil.com>
2020-09-11 20:07:47 +0200

    EINO veut que je peush encore parceque c la fin d'un capitre !!!!!!!!!!!!!

--- site/index.js
+++ site/index.js
@@ -2,7 +2,8 @@ var nb_canele = 0
 
 function NewCanele() {
     nb_canele += 1
-    document.getElementById("nb_canelé").innerHTML = "Paragraph changed!";
+    let paragraphe = "Vous avez "+ nb_canele.toString() +" canelés";
+    document.getElementById("nb_canelé").innerHTML = paragraphe;
 }
 
 function NewFour() {
