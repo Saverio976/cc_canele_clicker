eab22677a8ac7435604af8f4c104644253b24314
Saverio <personnex976@gamil.com>
2020-09-11 20:13:11 +0200

    Les mascotte c'est trop des fayottes ! (citation de moi, inventé par moi, reflechis par moi ;)

--- site/index.js
+++ site/index.js
@@ -1,17 +1,21 @@
 var nb_canele = 0
+var nb_four = 1
 
 function NewCanele() {
-    nb_canele += 1
+    nb_canele += nb_four;
     let paragraphe = "Vous avez "+ nb_canele.toString() +" canelés";
     document.getElementById("nb_canelé").innerHTML = paragraphe;
 }
 
 function NewFour() {
     if (nb_canele >= 25) {
-        nb_canele - 25
-        alert("You Got a New 'Four' !!")
+        nb_canele -= 25;
+        nb_four += 1;
+        let paragraphe = "Vous avez "+ nb_canele.toString() +" canelés";
+        document.getElementById("nb_canelé").innerHTML = paragraphe;
+        alert("You Got a New 'Four' !!");
     }
     else {
-        alert("You haven't The Number OF Canelé !")
+        alert("You haven't The Number OF Canelé !");
     }
 }
